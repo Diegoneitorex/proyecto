278aa9ee36dae393e6667d49dac82d5b1892c632
Diegoneitorex <diegoarf2448@gmail.com>
2025-01-02 14:51:47 -0400

    Base de Datos Actualizado

--- bd/bd_chat_whatsapp_02-01-2025.sql
+++ bd/bd_chat_whatsapp_02-01-2025.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-01-2025 a las 19:38:54
+-- Tiempo de generación: 02-01-2025 a las 19:49:45
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -49,10 +49,11 @@ INSERT INTO `users` (`id`, `nombre_apellido`, `email_user`, `password`, `imagen`
 (4, 'Cristian R.', 'cristian@hotmail.com', '$2y$10$xDZn40SPhfMagbYTsz4MZ.1L7XD.VN5OIcJCZzjrWWnvE5HjWtOci', '45d9649ddf.png', 'Activo', '09/09/2023 02:54 pm', NULL),
 (5, 'Valentina', 'Valentina@gmail.com', '$2y$10$kxPmU9mvpCM.KZgKUH0houoIHfD2w.xD2KD5czjjZxo6L53uGBihW', 'da4808ea74.png', 'Activo', '09/09/2023 02:57 pm', NULL),
 (6, 'Franco E.', 'franco@gmail.com', '$2y$10$5VLSB3NqFVjCOE.I8ooEY.kV9S1c96zDWDweaXH7RdG15v2p/RAIC', '17d760c7b0.jpeg', 'Activo', '09/09/2023 02:57 pm', NULL),
-(10, 'Diego Rivero', 'diegoarf2448@gmail.com', '$2y$10$lXb1MPVWmiTerzSXknXo0uqad6yI2JKpLzMQY38mPr/Q7/YxhBCi6', '2f0bf8361e.png', 'Inactiva', '02/01/2025 01:31 pm', '02/01/2025 01:34 pm'),
-(11, 'Alejandra Montoya', 'Alejitamon63@gmail.com', '$2y$10$g8gmypNUhklj/UEj67c9qOb0e0Nr0bR61nKZIOWxhHF2UJ2/xAxAe', '378e57d724.jpg', 'Activo', '02/01/2025 01:32 pm', NULL),
-(12, 'Valeria Guzman', 'valeriaguzman344@gmail.com', '$2y$10$dUk8kbtw.2Mte4OrFkJ7RO4OOJ7JJflXIHUtXSuAe9gBn6fF4y7pG', 'b60e824a38.jpg', 'Activo', '02/01/2025 01:34 pm', NULL),
-(13, 'Gabriela Peñalieto', 'gabyliieto78@gmail.com', '$2y$10$JgEm.HWXg2KCP5CNYKNs8u7Kp7gLrcuDjujdTkgt52J6SgdLVEA72', '9f2696730e.png', 'Activo', '02/01/2025 01:35 pm', NULL);
+(7, 'Diego Rivero', 'diegoarf2448@gmail.com', '$2y$10$lXb1MPVWmiTerzSXknXo0uqad6yI2JKpLzMQY38mPr/Q7/YxhBCi6', '2f0bf8361e.png', 'Inactiva', '02/01/2025 01:31 pm', '02/01/2025 01:34 pm'),
+(8, 'Alejandra Montoya', 'Alejitamon63@gmail.com', '$2y$10$g8gmypNUhklj/UEj67c9qOb0e0Nr0bR61nKZIOWxhHF2UJ2/xAxAe', '378e57d724.jpg', 'Activo', '02/01/2025 01:32 pm', NULL),
+(9, 'Valeria Guzman', 'valeriaguzman344@gmail.com', '$2y$10$dUk8kbtw.2Mte4OrFkJ7RO4OOJ7JJflXIHUtXSuAe9gBn6fF4y7pG', 'b60e824a38.jpg', 'Activo', '02/01/2025 01:34 pm', NULL),
+(10, 'Gabriela Peñalieto', 'gabyliieto78@gmail.com', '$2y$10$JgEm.HWXg2KCP5CNYKNs8u7Kp7gLrcuDjujdTkgt52J6SgdLVEA72', '9f2696730e.png', 'Activo', '02/01/2025 01:35 pm', NULL),
+(11, 'Maria Alejandra', 'marialejandra@gmail.com', '$2y$10$qoNHTerHTalBmTUXdfWr4O6xYg6hUmTPotp7SL6IRkyWEPUhUmrm6', '396903fd6e.jpg', 'Activo', '02/01/2025 01:48 pm', NULL);
 
 --
 -- Índices para tablas volcadas
@@ -72,7 +73,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
